1a55ef8ffd333796cb9eb7eb8f05f6b0940ba4b8
RenzWay <dikakamza@gmail.com>
2025-08-07 17:48:45 +0700

    try build for tes

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <main>
+    <p>pemai kau</p>
+    <p></p>
+
+    <button
+      class="bg-blue-600 p-4 rounded-sm hover:bg-blue-300 transition-all"
+      @click="console.log('ini ke klik')"
+    >
+      Click Me
+    </button>
+    <v-btn>ini button dari vuetify</v-btn>
+  </main>
+</template>
